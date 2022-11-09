71d353fa27018d5d77198e65501c8b5f487de33b
Farhan Tahmid <farhantahmid881@gmail.com>
2022-11-10 00:59:21 +0600

    algorithm ready for signup

--- api/OnAppAuth.py
+++ api/OnAppAuth.py
@@ -1,6 +1,7 @@
 from users.models import Members
 from django.http import HttpResponseServerError
 from django.db import DatabaseError
+from django.contrib.auth.models import User,auth
 class OnAppProcesses:
     def __init__(self) -> None:
         pass
@@ -10,6 +11,33 @@ class OnAppProcesses:
         except:
             return DatabaseError
         try:
-            pass
+            username=user.ieee_id
+            password='ieee@nSuSb' #Using a default pass at firs
+            if(User.objects.filter(username=username)):
+                return "Already Signedup"
+            else:
+                registeredUser=User.objects.create_user(username=username,email=email,password=password)
+                registeredUser.save()
+                return "success"
         except:
             return DatabaseError
+     
+    
+    def getUserData(email):
+        user:Members=Members.objects.get(email_ieee=email)
+        memberDataDict={
+                "status":"success",
+                "ieee_id":user.ieee_id,
+                "name":user.name,
+                "nsu_id":user.nsu_id,
+                "email_ieee":user.email_ieee,
+                "email_personal":user.email_personal,
+                "contact_no":user.contact_no,
+                "home_address":user.home_address,
+                "date_of_birth":user.date_of_birth,
+                "gender":user.gender,
+                "facebook_url":user.facebook_url,
+                "team":str(user.team),
+                "position":str(user.position)
+        }
+        return memberDataDict
--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,JsonResponse
 from users.models import Members
 from api.serializers import MembersSerializer
 from rest_framework.generics import ListAPIView
 from django.views.decorators.csrf import csrf_exempt
 from . import OnAppAuth
+from django.db import DatabaseError
 # Create your views here.
 
 
@@ -13,8 +14,19 @@ from . import OnAppAuth
 def signupAppUser(request):
     if request.method=="POST":
         email=request.POST.get("email_ieee")
-        OnAppAuth.OnAppProcesses.signup(email=email)
-        
+        try:
+            result=OnAppAuth.OnAppProcesses.signup(email=email)
+            if(result==DatabaseError):
+                return JsonResponse({"status":"database-failure"})
+            elif(result=="Already Signedup"):
+                return JsonResponse({"status":"already-signedup"})
+            elif(result=="success"):
+                data=OnAppAuth.OnAppProcesses.getUserData(email=email)
+                return JsonResponse(data=data)
+                
+        except:
+            return JsonResponse({"status":"code-failure"})
     else:
-        print("hocche na bhaia")
-    return HttpResponse("hoise connect")
+        return JsonResponse({"status":"connection-not secured"})
+
+    
