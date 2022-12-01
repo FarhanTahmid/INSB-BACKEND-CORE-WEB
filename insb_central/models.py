300e8a136db1d395f936382e9fc652b341886154
Farhan Tahmid <farhantahmid881@gmail.com>
2022-12-02 02:16:19 +0600

    models for events

--- insb_central/models.py
+++ insb_central/models.py
@@ -20,11 +20,32 @@ class Events(models.Model):
     def get_absolute_url(self):
         return reverse("registered_events", kwargs={"event_name": self.event_name})
 
-#list of the venues
+#list of the venues, primary key=id
 class Venue_List(models.Model):
     venue_name=models.CharField(null=False,blank=True,max_length=50)
+
+#Permission Criterias For an event, primary key=id
+class Permission_criteria(models.Model):
+    permission_name=models.CharField(null=False,blank=False,max_length=50)    
+
 
 # table for venues for specific events    
 class Event_Venue(models.Model):
     event_id=models.ForeignKey(Events,on_delete=models.CASCADE)
     venue_id=models.ForeignKey(Venue_List,on_delete=models.CASCADE)
+
+#table for event Proposal
+class Event_Proposal(models.Model):
+    event_id=models.ForeignKey(Events,on_delete=models.CASCADE)
+    event_proposal=models.FileField(upload_to="event_proposals",blank=True,null=True,default=None)
+    proposal_template=models.FileField(upload_to="event_proposals",blank=True,null=True,default=None)
+
+#Table for Media Links and Images
+class Media_Links(models.Model):
+    event_id=models.ForeignKey(Events,on_delete=models.CASCADE)
+    media_link=models.URLField(null=True,blank=True,max_length=300)
+class Media_Selected_Images(models.Model):
+    event_id=models.ForeignKey(Events,on_delete=models.CASCADE)
+    selected_image=models.ImageField(upload_to="event_selected_pictures",null=True,blank=True,default=None)
+
+
--- insb_port/urls.py
+++ insb_port/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('api/',include('api.urls')),
@@ -24,3 +26,4 @@ urlpatterns = [
     path('',include('port.urls',namespace='port')),
     path('insb_central/',include('insb_central.urls',namespace='insb_central')),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
