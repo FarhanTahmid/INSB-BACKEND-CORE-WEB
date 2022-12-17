bb6194a393c349946f8c45b5d86e7a03dfe46a1d
Farhan Tahmid <farhantahmid881@gmail.com>
2022-12-18 00:54:31 +0600

    edited comment

--- port/tests/tests_urls.py
+++ port/tests/tests_urls.py
@@ -1,4 +1,12 @@
+
+##THIS .py FILE TESTS IF ALL THE URLS ARE WORKING ON PORT APP
+
 from django.test import SimpleTestCase
+from django.urls import reverse,resolve
+from port.views import homepage
+
+
 class TestUrls(SimpleTestCase):
-    def test_urls_is_dissolved(self):
-        assert 1==2
+    def test_homepage_url(self):
+        url=reverse('port:homepage')
+        self.assertEquals(resolve(url).func,homepage)
