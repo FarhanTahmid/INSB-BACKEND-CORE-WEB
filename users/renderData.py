2eabe3294ce7e68644ce478964f4fe52f837404d
Farhan Tahmid <farhantahmid881@gmail.com>
2023-01-04 23:46:25 +0600

    profile picture done, delete previous upon new download

--- users/renderData.py
+++ users/renderData.py
@@ -1,6 +1,10 @@
 from users.models import Members
 from system_administration.models import adminUsers
 from port.models import Roles_and_Position,Teams
+import os
+from django.conf import settings
+from django.db import DatabaseError
+
 
 
 class LoggedinUser:
@@ -43,4 +47,88 @@ class LoggedinUser:
             except adminUsers.DoesNotExist:
                 return False
     
+    
+    def change_profile_picture(self,picture_file):
+        try:
+            get_user=Members.objects.get(ieee_id=self.user.username)
+            previous_profile_picture=settings.MEDIA_ROOT+str(get_user.user_profile_picture)
+            print(get_user.user_profile_picture)
+            print(f"Previous={previous_profile_picture}")
+            print(f"Default={settings.MEDIA_ROOT+'user_profile_pictures/default_profile_picture.png'}")
+            
+            if(previous_profile_picture!=(settings.MEDIA_ROOT+'user_profile_pictures/default_profile_picture.png')):
+                
+                if(os.path.isfile(previous_profile_picture)):
+                    
+                    try:
+                        os.remove(previous_profile_picture)
+                        
+                        get_user.user_profile_picture=picture_file
+                        get_user.save()
+                        return True
+                    except OSError:
+                        return False
+
+                else:
+                    get_user.user_profile_picture=picture_file
+                    get_user.save()
+                    return True
+            
+            else:
+                get_user.user_profile_picture=picture_file
+                get_user.save()
+                return True
+                
+        except Members.DoesNotExist:
+            try:
+                get_user=adminUsers.objects.get(username=self.user.username)
+                previous_profile_picture=settings.MEDIA_ROOT+str(get_user.profile_picture)
+                
+                if(previous_profile_picture!=(settings.MEDIA_ROOT+'Admin/admin_profile_pictures/default_profile_picture.png')):
+                    
+                    if(os.path.isfile(previous_profile_picture)):
+                        try:
+                            os.remove(previous_profile_picture)
+                            get_user.profile_picture=picture_file
+                            get_user.save()
+                            return True
+                        except OSError:
+                            return False
+                    else:
+                        get_user.profile_picture=picture_file
+                        get_user.save()
+                        return True
+                else:
+                    get_user.profile_picture=picture_file
+                    get_user.save()
+                    return True
+                
+            except adminUsers.DoesNotExist:
+                return False 
+        except ValueError:
+            try:
+                get_user=adminUsers.objects.get(username=self.user.username)
+                previous_profile_picture=settings.MEDIA_ROOT+str(get_user.profile_picture)
+                
+                if(previous_profile_picture!=(settings.MEDIA_ROOT+'\Admin\admin_profile_pictures')):
+                    
+                    if(os.path.isfile(previous_profile_picture)):
+                        try:
+                            os.remove(previous_profile_picture)
+                            get_user.profile_picture=picture_file
+                            get_user.save()
+                            return True
+                        except OSError:
+                            return False
+                    else:
+                        get_user.profile_picture=picture_file
+                        get_user.save()
+                        return True
+                else:
+                    get_user.profile_picture=picture_file
+                    get_user.save()
+                    return True
+                
+            except adminUsers.DoesNotExist:
+                return False 
     
