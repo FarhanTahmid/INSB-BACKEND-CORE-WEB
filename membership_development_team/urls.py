b399d766d1ed5c9cd024ca1faa4a8979cf70c507
Farhan Tahmid <farhantahmid881@gmail.com>
2023-01-10 22:36:06 +0600

    minor bug fixes

--- membership_development_team/urls.py
+++ membership_development_team/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
    path('membership_renewal/',views.membership_renewal,name="membership_renewal"),
    path('membership_renewal/session/<str:pk>',views.renewal_session_data,name="renewal_session_data"),
    path('renewal_form/<str:pk>',views.membership_renewal_form,name="renewal_form"),
+   path('renewal_form/<str:pk>/success',views.membership_renewal_form_success,name="renewal_form_success"),
    path('renewal_request/<str:pk>/<str:request_id>',views.renewal_request_details,name="request_details"), #using renewal request tables primary key 'id' as url because its the only unique matter in renewal process as no ieee id could be used
    path('export_excel_renewal_request/<str:session_id>',views.generateExcelSheet_renewal_requestList,name="export_excel_renewal_request"), #generate excel for renewal request
    path('data_access',views.data_access,name="data_access"),
