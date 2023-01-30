b6dd9026f4f1c5cab8f65dd4dd8c8ba9160dafa1
Suraiya Islam Meem <suraiya.meem@northsouth.edu>
2023-01-30 16:38:21 +0600

    meeting minutes model update

--- meeting_minutes/admin.py
+++ meeting_minutes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import team_meeting_minutes
 # Register your models here.
+@admin.register(team_meeting_minutes)
+class team_meeting_minutes(admin.ModelAdmin):
+    list_display=['id','team_id','meeting_title']
--- meeting_minutes/models.py
+++ meeting_minutes/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from port.models import Teams
 # Create your models here.
 
 # meeting minutes' information
-class meeting_minute(models.Model):
+class team_meeting_minutes(models.Model):
+    team_id=models.ForeignKey(Teams,null=False,blank=False,on_delete=models.CASCADE)
     meeting_title=models.CharField(null=False,blank=False,max_length=150)
     meeting_date=models.DateField(null=True,blank=True,auto_now_add=False)
     meeting_venue=models.CharField(null=False,blank=False,max_length=150)
@@ -16,9 +18,9 @@ class meeting_minute(models.Model):
     meeting_discussion=models.CharField(null=False,blank=False,max_length=3000)
 #team foreign key from each chapters
     class Meta:
-        verbose_name="Registered Meetings"
+        verbose_name="Registered Team Meetings"
     
     def __str__(self) -> str:
         return self.meeting_title
     def get_absolute_url(self):
-        return reverse("Registered Meetings", kwargs={"meeting_title": self.meeting_title})
+        return reverse("team_meetings", kwargs={"meeting_title": self.meeting_title})
