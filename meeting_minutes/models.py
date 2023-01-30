f6208ce89ece4d7ecf87f2d1de767a750401072d
Suraiya Islam Meem <suraiya.meem@northsouth.edu>
2023-01-30 17:11:55 +0600

    branch made in mm models

--- meeting_minutes/models.py
+++ meeting_minutes/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.urls import reverse
-from port.models import Teams
+from port.models import Teams, Chapters_Society_and_Affinity_Groups
 # Create your models here.
 
 # meeting minutes' information
 class team_meeting_minutes(models.Model):
-    team_id=models.ForeignKey(Teams,null=False,blank=False,on_delete=models.CASCADE)
+    team_id=models.ForeignKey(Teams,null=False,blank=False,on_delete=models.CASCADE) #team foreign key from each chapters
     meeting_title=models.CharField(null=False,blank=False,max_length=150)
     meeting_date=models.DateField(null=True,blank=True,auto_now_add=False)
+    meeting_time=models.TimeField(null=True,blank=True,auto_now_add=False)
     meeting_venue=models.CharField(null=False,blank=False,max_length=150)
     meeting_present=models.CharField(null=False,blank=False,max_length=150)
     meeting_excused=models.CharField(null=False,blank=False,max_length=150)
@@ -16,11 +17,33 @@ class team_meeting_minutes(models.Model):
     meeting_agendas=models.CharField(null=False,blank=False,max_length=150)
     meeting_approval=models.CharField(null=False,blank=False,max_length=150)
     meeting_discussion=models.CharField(null=False,blank=False,max_length=3000)
-#team foreign key from each chapters
+
     class Meta:
-        verbose_name="Registered Team Meetings"
+        verbose_name="Registered Team Meeting"
     
     def __str__(self) -> str:
         return self.meeting_title
     def get_absolute_url(self):
         return reverse("team_meetings", kwargs={"meeting_title": self.meeting_title})
+
+class branch_meeting_minutes(models.Model):
+    branch_or_society_id=models.ForeignKey(Chapters_Society_and_Affinity_Groups, null=False, blank=False, on_delete=models.CASCADE)
+    meeting_title=models.CharField(null=False,blank=False,max_length=150)
+    meeting_date=models.DateField(null=True,blank=True,auto_now_add=False)
+    meeting_time=models.TimeField(null=True,blank=True,auto_now_add=False)
+    meeting_venue=models.CharField(null=False,blank=False,max_length=150)
+    meeting_present=models.CharField(null=False,blank=False,max_length=150)
+    meeting_excused=models.CharField(null=False,blank=False,max_length=150)
+    meeting_absent=models.CharField(null=False,blank=False,max_length=150)
+    meeting_called_by=models.CharField(null=False,blank=False,max_length=150)
+    meeting_agendas=models.CharField(null=False,blank=False,max_length=150)
+    meeting_approval=models.CharField(null=False,blank=False,max_length=150)
+    meeting_discussion=models.CharField(null=False,blank=False,max_length=3000)
+#Chapters_Society_and_Affinity_Groups foreign key from each chapters
+    class Meta:
+        verbose_name="Branch - Society Meeting"
+    
+    def __str__(self) -> str:
+        return self.meeting_title
+    def get_absolute_url(self):
+        return reverse("branch_meeting_minutes", kwargs={"meeting_title": self.meeting_title})
