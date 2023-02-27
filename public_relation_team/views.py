d03b35e2c40b0803bc19a0f694d021023319c72c
Farhan Tahmid <farhantahmid881@gmail.com>
2023-02-27 23:13:44 +0600

    minor fixes in frontend

--- public_relation_team/views.py
+++ public_relation_team/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
 # Create your views here.
 
 def team_home_page(request):
     return render(request,"team_homepage.html")
+
