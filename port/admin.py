5985bc0d2ed87bee65a6a5a70777554615552ff3
Farhan Tahmid <farhantahmid881@gmail.com>
2023-09-06 21:15:10 +0600

    created tables for society positions, teams, created app for pes sbc

--- port/admin.py
+++ port/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Teams,Roles_and_Position,Chapters_Society_and_Affinity_Groups
+from . models import Teams,Roles_and_Position,Chapters_Society_and_Affinity_Groups,Society_Chapters_AG_Roles_and_Positions,Society_Chapters_AG_Teams
 # Register your models here.
 @admin.register(Teams)
 class Teams(admin.ModelAdmin):
@@ -10,3 +10,13 @@ class Roles(admin.ModelAdmin):
 @admin.register(Chapters_Society_and_Affinity_Groups)
 class Chapter_Society(admin.ModelAdmin):
     list_display=['id','group_name','primary']
+
+@admin.register(Society_Chapters_AG_Roles_and_Positions)
+class Society_Chapter_Roles(admin.ModelAdmin):
+    list_display=[
+        'id','chapter_id','position_name','is_faculty_position','is_eb_position','is_officer_position'
+    ]
+
+@admin.register(Society_Chapters_AG_Teams)
+class Society_Chapter_Teams(admin.ModelAdmin):
+    list_display = ['id','chapter_id','team_name']
--- port/models.py
+++ port/models.py
@@ -29,3 +29,30 @@ class Roles_and_Position(models.Model):
         verbose_name='Registered positions'
     def __str__(self) -> str:
         return self.role
+    
+class Society_Chapters_AG_Teams(models.Model):
+    chapter_id=models.ForeignKey(Chapters_Society_and_Affinity_Groups,on_delete=models.CASCADE,null=False,blank=False)
+    team_name=models.CharField(null=False,blank=False,max_length=50)
+    
+    class Meta:
+        verbose_name="Society, Chapters & AG Teams"
+        
+    def __int__(self)->int:
+        return self.pk
+    
+    
+class Society_Chapters_AG_Roles_and_Positions(models.Model):
+    id=models.IntegerField(primary_key=True)
+    chapter_id=models.ForeignKey(Chapters_Society_and_Affinity_Groups,null=False,blank=False,on_delete=models.CASCADE)
+    position_name=models.CharField(null=False,blank=False,max_length=50)
+    is_faculty_position=models.BooleanField(null=False,blank=False,default=False)
+    is_eb_position=models.BooleanField(null=False,blank=False,default=False)
+    is_officer_position=models.BooleanField(null=False,blank=False,default=False)
+    
+    class Meta:
+        verbose_name="Society, Chapters & Affinity Group Registered Positions"
+    def __int__(self) -> int:
+        return self.id
+
+    
+    
