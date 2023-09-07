faf343fdb462287c9e71abbcb5b6706ad20add07
Farhan Tahmid <farhantahmid881@gmail.com>
2023-09-07 19:43:42 +0600

    changes pes sbc models according to roles and position and teams

--- ieee_nsu_sb_pes_sbc/models.py
+++ ieee_nsu_sb_pes_sbc/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from users.models import Members
-
+from port.models import Teams,Roles_and_Position
 # Create your models here.
 
 class PesMembers(models.Model):
     ieee_id=models.ForeignKey(Members,null=False,blank=False,on_delete=models.CASCADE)
-    # position=models.ForeignKey(Society_Chapters_AG_Roles_and_Positions,null=False,blank=False,on_delete=models.CASCADE)
-    # team=models.ForeignKey(Society_Chapters_AG_Teams,null=True,blank=True,on_delete=models.CASCADE)
+    position=models.ForeignKey(Roles_and_Position,null=True,blank=True,on_delete=models.CASCADE)
+    team=models.ForeignKey(Teams,null=True,blank=True,on_delete=models.CASCADE)
 
     class Meta:
         verbose_name="PES Registered Members"
