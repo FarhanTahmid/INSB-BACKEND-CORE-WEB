d543ce31ba734b59ba06064bb57f5a836ba1f748
Farhan Tahmid <farhantahmid881@gmail.com>
2023-09-18 18:22:53 +0600

    issue solved of wave in frontend

--- main_website/urls.py
+++ main_website/urls.py
@@ -5,7 +5,7 @@ app_name = "main_website"
 
 urlpatterns = [
     path('',views.homepage,name="homepage"),
-    path('all-events/',views.All_Events,name='all-events'),
+    path('events/',views.all_events,name="all_events"),
     path('events/<int:event_id>',views.Event_Details,name='event-details'),
     path('research-papers/',views.Research_Paper,name="research_papers"),
     path('ieeensu_ras_sbc/',views.rasPage,name="ras_home"),
