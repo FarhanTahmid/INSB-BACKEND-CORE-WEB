ab73964adf4d9c6b464992859df96171a24582ff
Sakib2186 <skmdsakib2186@gmail.com>
2023-10-06 23:06:43 +0600

    Done with Manage Team for Media

--- central_branch/static/table_search.js
+++ central_branch/static/table_search.js
@@ -1,4 +1,3 @@
-getPagination('#table-id2');
 getPagination('#table-id');
 	$('#maxRows').trigger('change');
 	function getPagination (table){
@@ -63,13 +62,12 @@ getPagination('#table-id');
     
 	}	
 
-
 // SI SETTING
-$(function(){
+/**$(function(){
 	// Just to append id number for each row  
 default_index();
 					
-});
+});*/
 
 //ROWS SHOWING FUNCTION
 function showig_rows_count(maxRows, pageNum, totalRows) {
@@ -81,7 +79,7 @@ function showig_rows_count(maxRows, pageNum, totalRows) {
 }
 
 // CREATING INDEX
-function default_index() {
+/**function default_index() {
   $('table tr:eq(0)').prepend('<th> SL No. </th>')
 
 					var id = 0;
@@ -90,10 +88,11 @@ function default_index() {
 						id++
 						$(this).prepend('<td>'+id+'</td>');
 					});
-}
+}*/
 
 // All Table search script
 function FilterkeyWord_all_table() {
+
   
 // Count td if you want to search on all table instead of specific column
 
@@ -138,3 +137,51 @@ function FilterkeyWord_all_table() {
       $('#maxRows').trigger('change');
     }
 }
+
+// All Table search script
+function FilterkeyWord_all_table2() {
+  
+  // Count td if you want to search on all table instead of specific column
+  
+    var count = $('.table').children('tbody').children('tr:first-child').children('td').length; 
+  
+          // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("search_input_all2");
+    var input_value =     document.getElementById("search_input_all2").value;
+          filter = input.value.toLowerCase();
+    if(input_value !=''){
+          table = document.getElementById("table-id2");
+          tr = table.getElementsByTagName("tr");
+  
+          // Loop through all table rows, and hide those who don't match the search query
+          for (i = 1; i < tr.length; i++) {
+            
+            var flag = 0;
+             
+            for(j = 0; j < count; j++){
+              td = tr[i].getElementsByTagName("td")[j];
+              if (td) {
+               
+                  var td_text = td.innerHTML;  
+                  if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+                  //var td_text = td.innerHTML;  
+                  //td.innerHTML = 'shaban';
+                    flag = 1;
+                  } else {
+                    //DO NOTHING
+                  }
+                }
+              }
+            if(flag==1){
+                       tr[i].style.display = "";
+            }else {
+               tr[i].style.display = "none";
+            }
+          }
+      }else {
+        //RESET TABLE
+        $('#maxRows').trigger('change');
+      }
+  }
+  
