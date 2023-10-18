f69425f003e1bb95f6bbdcf04eb3ddf57f264de1
Farhan Tahmid <farhantahmid881@gmail.com>
2023-10-18 21:52:40 +0600

    created models for ras about section

--- main_website/models.py
+++ main_website/models.py
@@ -30,7 +30,30 @@ class BannerPictureWithStat(models.Model):
     
     def __str__(self) -> str:
         return str(self.pk)
+
+# Table for SOCIETY AND AG's
+# RAS SBC
+class Ras_Sbc(models.Model):
+    ras_banner_image=ResizedImageField(upload_to='main_website_files/RAS/banner_picture')
+    about_ras=models.TextField(null=False,blank=False,max_length=1000)
+    mission_vision=models.TextField(null=False,blank=False,max_length=1000)
+    mission_vision_picture=ResizedImageField(upload_to='main_website_files/RAS/mission_vision_picture')
+    what_is_ras=models.TextField(null=True,blank=True,max_length=300)
+    why_join_ras=models.TextField(null=True,blank=True,max_length=300)
+    what_activities=models.TextField(null=True,blank=True,max_length=300)
+    how_to_join=models.TextField(null=True,blank=True,max_length=300)
     
+    class Meta:
+        verbose_name="RAS SBC Informations"
+    
+    def __str__(self) -> str:
+        return str(self.pk)
+
+
+
+
+
+
 #Table for Research Papers
 class Research_Papers(models.Model):
     title = models.CharField(null=False,blank=False,max_length=500)
