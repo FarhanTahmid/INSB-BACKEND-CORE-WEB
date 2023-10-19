547a62e825262990266fbbf1f0166064458cbcd6
Sakib2186 <skmdsakib2186@gmail.com>
2023-10-19 23:44:03 +0600

    Added model for the Graphics Team

--- graphics_team/admin.py
+++ graphics_team/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Graphics_Link,Graphics_Banner_Image
 
 # Register your models here.
+@admin.register(Graphics_Link)
+class Graphics_Link(admin.ModelAdmin):
+    list_display = ['event_id','graphics_link']
+@admin.register(Graphics_Banner_Image)
+class Graphics_Banner_Image(admin.ModelAdmin):
+    list_display = ['event_id','selected_image']
