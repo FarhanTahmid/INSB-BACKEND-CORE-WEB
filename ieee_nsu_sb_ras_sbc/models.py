dca455dbd889133b209d20ebdeda24ca3580a4c3
Farhan Tahmid <farhantahmid881@gmail.com>
2023-10-21 15:02:53 +0600

    tested rich text on ras page, works

--- ieee_nsu_sb_ras_sbc/models.py
+++ ieee_nsu_sb_ras_sbc/models.py
@@ -6,13 +6,13 @@ from ckeditor.fields import RichTextField
 # RAS SBC
 class Ras_Sbc(models.Model):
     ras_banner_image=ResizedImageField(upload_to='main_website_files/RAS/banner_picture')
-    about_ras=RichTextField(null=False,blank=False,max_length=1000)
-    mission_vision=models.TextField(null=False,blank=False,max_length=1000)
+    about_ras=RichTextField(null=False,blank=False,max_length=2000)
+    mission_vision=RichTextField(null=False,blank=False,max_length=2000)
     mission_vision_picture=ResizedImageField(upload_to='main_website_files/RAS/mission_vision_picture')
-    what_is_ras=models.TextField(null=True,blank=True,max_length=300)
-    why_join_ras=models.TextField(null=True,blank=True,max_length=300)
-    what_activities=models.TextField(null=True,blank=True,max_length=300)
-    how_to_join=models.TextField(null=True,blank=True,max_length=300)
+    what_is_ras=RichTextField(null=True,blank=True,max_length=1000)
+    why_join_ras=RichTextField(null=True,blank=True,max_length=1000)
+    what_activities=RichTextField(null=True,blank=True,max_length=1000)
+    how_to_join=RichTextField(null=True,blank=True,max_length=100)
     
     #RAS Contact Info
     address=models.CharField(null=True,blank=True,max_length=200)
