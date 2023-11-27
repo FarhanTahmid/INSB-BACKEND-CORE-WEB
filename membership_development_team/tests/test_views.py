b0a000fe44df27b3878b640a038e845f744b6410
Sakib2186 <skmdsakib2186@gmail.com>
2023-11-27 18:59:19 +0600

    ..

--- membership_development_team/tests/test_views.py
+++ membership_development_team/tests/test_views.py
@@ -4,7 +4,8 @@ from users.models import Members
 from port.models import Teams,Roles_and_Position
 from django.contrib.auth.models import User
 from membership_development_team.models import Renewal_Sessions,Renewal_requests,Renewal_Form_Info,Portal_Joining_Requests
-import json
+import json,datetime
+from recruitment.models import recruitment_session
 
 class TestViews(TestCase):
 
@@ -14,6 +15,7 @@ class TestViews(TestCase):
         self.md_team_homepage_url = reverse('membership_development_team:md_team_homepage')
         self.insb_member_list_url = reverse('membership_development_team:members_list')
         self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.member_details = reverse('membership_development_team:member_details',args=[1])
         Teams.objects.create(team_name="Membership Development",primary=7)
         Members.objects.create(ieee_id=1, name='Co-ordinator 1',nsu_id=1234, position=Roles_and_Position.objects.create(role="Co-ordinator",pk=9))
         
@@ -50,7 +52,22 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, f'/portal/users/login?next=/portal/membership_development_team/members/')
     
-    
+    def test_md_team_member_details_GET(self):
+        recruitment_session.objects.create(session="Fall2023")
+        Renewal_Sessions.objects.create(session_name="Fall2023",session_time=datetime.datetime.now())
+        self.client.login(username='testuser',password='testpassword')
+        response = self.client.post(self.member_details)
+        self.assertEqual(response.status_code, 200)
+
+        
+        self.assertIn('member_data', response.context)
+        self.assertIn('dob', response.context)
+        self.assertIn('sessions', response.context)
+        self.assertIn('renewal_session', response.context)
+        self.assertIn('media_url', response.context)
+        self.assertIn('active_status', response.context)
+        self.assertIn('user_data', response.context)
+        
     
         
 
