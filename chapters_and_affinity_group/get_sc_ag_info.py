d2d590e1ce06a9dfdd3b449c86178ef4ac150cbd
Farhan Tahmid <farhantahmid881@gmail.com>
2023-11-28 12:31:29 +0600

    created members model

---
+++ chapters_and_affinity_group/get_sc_ag_info.py
@@ -0,0 +1,31 @@
+import logging
+from port.models import Chapters_Society_and_Affinity_Groups
+from .models import SC_AG_Members
+from datetime import datetime
+from system_administration.system_error_handling import ErrorHandling
+import traceback
+from django.contrib import messages
+from django.http import Http404
+
+class SC_AG_Info:
+    
+    logger=logging.getLogger(__name__)
+    
+    def get_sc_ag_details(request,sc_ag_primary):
+        '''This returns the SC_AG details as an object'''
+        try:
+            return Chapters_Society_and_Affinity_Groups.objects.get(primary=sc_ag_primary)
+        except Exception as e:
+            SC_AG_Info.logger.error("An error occurred at {datetime}".format(datetime=datetime.now()), exc_info=True)
+            ErrorHandling.saveSystemErrors(error_name=e,error_traceback=traceback.format_exc())
+            return Http404
+    
+    def get_sc_ag_members(request,sc_ag_primary):
+        '''This returns the members of the sc_ag as a query set.'''
+        get_sc_ag=SC_AG_Info.get_sc_ag_details(request,sc_ag_primary)
+        try:
+            SC_AG_Members.objects.filter(sc_ag=Chapters_Society_and_Affinity_Groups.objects.get(pk=get_sc_ag.pk))
+        except Exception as e:
+            SC_AG_Info.logger.error("An error occurred at {datetime}".format(datetime=datetime.now()), exc_info=True)
+            ErrorHandling.saveSystemErrors(error_name=e,error_traceback=traceback.format_exc())
+            return Http404
--- chapters_and_affinity_group/views.py
+++ chapters_and_affinity_group/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from port.renderData import PortData
+from .get_sc_ag_info import SC_AG_Info
+
 # Create your views here.
 
 def sc_ag_homepage(request,primary):
     sc_ag=PortData.get_all_sc_ag(request=request)
 
+    get_sc_ag_info=SC_AG_Info.get_sc_ag_details(request,primary)
+    
+    
     context={
         'all_sc_ag':sc_ag,
+        'sc_ag_info':get_sc_ag_info
     }
     return render(request,'Homepage/homepage.html',context)
