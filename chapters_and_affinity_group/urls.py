61270f8607747b07cd33c503252a3c7b9913335a
Farhan Tahmid <farhantahmid881@gmail.com>
2023-12-01 12:07:15 +0600

    panel frontend ready

--- chapters_and_affinity_group/urls.py
+++ chapters_and_affinity_group/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('<str:primary>/members',views.sc_ag_members,name='sc_ag_members'),
     path('<str:primary>/panels',views.sc_ag_panels,name="sc_ag_panels"),
     path('<str:primary>/panels/<str:panel_pk>',views.sc_ag_panel_details,name="sc_ag_panel_details"),
+    path('<str:primary>/panels/<str:panel_pk>/officers',views.sc_ag_panel_details_officers_tab,name="sc_ag_panel_details_officers"),
+    path('<str:primary>/panels/<str:panel_pk>/volunteers',views.sc_ag_panel_details_volunteers_tab,name="sc_ag_panel_details_volunteers"),
+    path('<str:primary>/panels/<str:panel_pk>/alumni',views.sc_ag_panel_details_alumni_members_tab,name="sc_ag_panel_details_alumni"),
+
+    
+
 ]
