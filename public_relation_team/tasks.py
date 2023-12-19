69ef70c829cdb7a6178f6afca1dc3384ec1345b6
Sakib2186 <skmdsakib2186@gmail.com>
2023-12-19 11:18:47 +0600

    done with email schedule but for direct emailing multiple attachment is not ready yet

--- public_relation_team/tasks.py
+++ public_relation_team/tasks.py
@@ -2,12 +2,17 @@ from celery import shared_task
 from insb_port.celery import app
 from public_relation_team.render_email import PRT_Email_System
 import json
-import base64
+from .models import Email_Attachements
+
 
 
 @shared_task
-def send_scheduled_email(to_email_list, cc_email_list, bcc_email_list, subject, mail_body, attachment):
+def send_scheduled_email(to_email_list, cc_email_list, bcc_email_list, subject, mail_body,unique_task_name_json):
     to_email_list = json.loads(to_email_list)
     cc_email_list = json.loads(cc_email_list)
     bcc_email_list = json.loads(bcc_email_list)
-    PRT_Email_System.send_email(to_email_list, cc_email_list, bcc_email_list, subject, mail_body,attachment)
+    unique_task_name = json.loads(unique_task_name_json)
+    get_list_of_attachments = Email_Attachements.objects.filter(email_name = unique_task_name)
+    print(get_list_of_attachments)
+    is_scheduled= True
+    PRT_Email_System.send_email(to_email_list, cc_email_list, bcc_email_list, subject, mail_body,is_scheduled,get_list_of_attachments)
