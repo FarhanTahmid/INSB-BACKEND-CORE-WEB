429f901147749b7941707d9be2fb6fe145dd3a2a
Arman Mokammel <armanmokammel@gmail.com>
2023-12-21 04:08:25 +0600

    Fixed and completed add_note feature for CWP Team

--- content_writing_and_publications_team/forms.py
+++ content_writing_and_publications_team/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from  .models import *
+from ckeditor.widgets import CKEditorWidget
 
 class Content_Form(forms.ModelForm):
+    textarea_id_counter = 0
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['notes'].widget = CKEditorWidget(attrs={'id': Content_Form.get_textarea_next_id()})
+
+    def get_textarea_next_id():
+        id_string = 'txtarea_id_' + str(Content_Form.textarea_id_counter)
+        Content_Form.textarea_id_counter += 1
+        return id_string
+    
     class Meta:
         model = Content_Notes
         fields = ['notes']
