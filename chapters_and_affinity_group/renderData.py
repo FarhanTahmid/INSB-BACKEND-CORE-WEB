de02fca89fe48aabe32789b82aece788c1178ba9
Sakib2186 <skmdsakib2186@gmail.com>
2024-01-05 22:47:20 +0600

    fixed

--- chapters_and_affinity_group/renderData.py
+++ chapters_and_affinity_group/renderData.py
@@ -453,22 +453,12 @@ class Sc_Ag:
                 or len(how_to_join)==0):
                 return True
             else:
-                    return False
+                return False
         except Exception as e:
             Sc_Ag.logger.error("An error occurred at {datetime}".format(datetime=datetime.now()), exc_info=True)
             ErrorHandling.saveSystemErrors(error_name=e,error_traceback=traceback.format_exc())
             messages.error(request,f"Could not update the main page of Sc_Ag group = {primary}!")
             return False
-        
-    def process_ckeditor_content(ckeditor_html):
-        # Parse the HTML content with Beautiful Soup
-        soup = BeautifulSoup(ckeditor_html, 'html.parser')
-
-        # Extract the text content without HTML tags
-        text_content = soup.get_text()
-
-        # Now, 'text_content' contains only the actual content without HTML tags
-        return text_content
         
     def process_ckeditor_content(ckeditor_html):
         # Parse the HTML content with Beautiful Soup
