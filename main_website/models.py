4b28f9c04e379712c306f198ff2a5706c26db506
Farhan Tahmid <farhantahmid881@gmail.com>
2024-01-10 21:15:49 +0600

    start of volunteer recognition

--- main_website/models.py
+++ main_website/models.py
@@ -242,3 +242,13 @@ class Toolkit(models.Model):
         verbose_name="Toolkit"
     def __str__(self) -> str:
         return self.title
+
+class VolunteerOfTheMonth(models.Model):
+    ieee_id=models.ForeignKey(Members,null=False,blank=False,on_delete=models.CASCADE)
+    contributions=RichTextField(null=True,blank=True,max_length=200)
+    
+    class Meta:
+        verbose_name="Volunteer Of the Month"
+    def __str__(self) -> str:
+        return str(self.ieee_id)
+    
