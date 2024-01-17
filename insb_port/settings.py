e17b06a2ce25a4abd7b3b3f4d730b33e344641e2
Farhan Tahmid <farhantahmid881@gmail.com>
2024-01-17 19:58:10 +0600

    email config fixed

--- insb_port/settings.py
+++ insb_port/settings.py
@@ -212,11 +212,12 @@ handler500='central_branch.views.custom_500'
 
 #EMAIL SETTINGS
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
+EMAIL_HOST='mail.ieeensusb.org'
+EMAIL_PORT='465'
 EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASSWORD')
-EMAIL_USE_TLS=True
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS=False
 
 # RESIZING IMAGE
 DJANGORESIZED_DEFAULT_SIZE = [1920, 1080]
