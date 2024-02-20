5901e8f4ce140b583683fef336adeaffde891ea2
farhana <farhana17mar@gmail.com>
2024-02-20 21:44:51 +0600

    task home done

--- task_assignation/views.py
+++ task_assignation/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def task_homepage(request):
+        return render(request,"task_homepage.html")
+def task_home(request):
+        return render(request,"task_home.html")
