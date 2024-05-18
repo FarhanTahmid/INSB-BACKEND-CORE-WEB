ec76006acd11898dfa89357f2757ea57de4029ee
Sakib2186 <skmdsakib2186@gmail.com>
2024-05-18 10:17:17 +0600

    uncommented email.send()

--- task_assignation/renderData.py
+++ task_assignation/renderData.py
@@ -1337,7 +1337,7 @@ This is an automated message. Do not reply
                                 email_from,
                                 email_to
                                 )
-            #email.send()
+            email.send()
 
             task_log_message = f'Task Name: {task.title}, {task.task_created_by} just added a comment on member, {member.name}({member_id}), work'
             #saving logs
@@ -1413,7 +1413,7 @@ This is an automated message. Do not reply
                                 email_from,
                                 email_to
                                 )
-            #email.send()
+            email.send()
             task_log_message = f'Task Name: {task.title}, task checked completed by {user_name} and notified to task assignee'
             #setting message
             Task_Assignation.save_task_logs(task,task_log_message)
@@ -1474,7 +1474,7 @@ This is an automated message. Do not reply
                                     email_from,
                                     email_to
                                     )
-            #email.send()
+            email.send()
             task_log_message = f'Task Name: {task.title}, task creation email sent to {member.name}({member.ieee_id})'
             #setting message
             Task_Assignation.save_task_logs(task,task_log_message)
@@ -2255,7 +2255,7 @@ This is an automated message. Do not reply
                                     email_from,
                                     email_to
                                     )
-            #email.send()
+            email.send()
             task_log_message = f'Task Name: {task.title}, task completion email sent to {member.name}({member.ieee_id})'
             #setting message
             Task_Assignation.save_task_logs(task,task_log_message)
