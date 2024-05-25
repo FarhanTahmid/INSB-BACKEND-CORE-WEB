1dfb43871480b0d12c8ac790c0d0ccb3d2e8d5e2
Arman Mokammel <armanmokammel@gmail.com>
2024-05-26 01:07:18 +0600

    disable test code

--- users/views.py
+++ users/views.py
@@ -204,7 +204,7 @@ def dashboard(request):
         monthly_members = Member_Task_Point.objects.filter(completion_date__month=datetime.now().month, completion_date__year=datetime.now().year).order_by('-completion_points','member')
         monthly_top_3_members = {}
 
-        msg = "" + str(datetime.now()) + "    " + str(monthly_members)
+        # msg = "" + str(datetime.now()) + "    " + str(monthly_members)
 
         for member in monthly_members :
             if (not member.member in monthly_top_3_members.keys()):
@@ -212,14 +212,12 @@ def dashboard(request):
             else:
                 monthly_top_3_members[member.member][1] += member.completion_points
 
-        msg = msg + "\n\n" + str(monthly_top_3_members)
-
-        new_error=SystemErrors.objects.create(
-                date_time=datetime.now(),
-                error_name="error_name",
-                error_traceback=msg
-            )
-        new_error.save()
+        # new_error=SystemErrors.objects.create(
+        #         date_time=datetime.now(),
+        #         error_name="error_name",
+        #         error_traceback=msg
+        #     )
+        # new_error.save()
                 
         # Get the SC & AGS
         sc_ag=PortData.get_all_sc_ag(request=request)
