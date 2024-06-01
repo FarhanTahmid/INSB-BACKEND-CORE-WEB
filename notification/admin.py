bca18514df260d1666e6d1757eaa090cee8f771a
Farhan Tahmid <farhantahmid881@gmail.com>
2024-06-01 11:26:02 +0600

    created models for notification

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(NotificationTypes)
+class NotificationTypes(admin.ModelAdmin):
+    list_display=['pk','type']
+
+@admin.register(Notifications)
+class Notifications(admin.ModelAdmin):
+    list_display=[
+         'pk','type','created_by','general_message','timestamp'
+    ]
+    
+@admin.register(MemberNotifications)
+class MemberNotifications(admin.ModelAdmin):
+    list_display=['notification','member','is_read']
