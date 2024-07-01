11b02dc894174e1e0a7c44e2ca283806f94fe8bd
Sakib2186 <skmdsakib2186@gmail.com>
2024-07-02 00:10:40 +0600

    .

--- notification/views.py
+++ notification/views.py
@@ -148,6 +148,7 @@ def fetch_notifications(request):
                                         
                 },
                 'is_read': member_notification.is_read,
+                'notification_type_image':str(settings.MEDIA_URL)+str(member_notification.notification.type.type_icon),
             }
        
             notifications.append(dic)
