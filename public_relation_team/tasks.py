d13a63eca5fd80087abab4ad2ab056ce677974c0
Arman Mokammel <armanmokammel@gmail.com>
2024-08-25 17:47:14 +0600

    .

--- public_relation_team/tasks.py
+++ public_relation_team/tasks.py
@@ -15,7 +15,6 @@ def send_scheduled_email(id,unique_task_name_json):
     # cc_email_list = json.loads(cc_email_list)
     # bcc_email_list = json.loads(bcc_email_list)
     unique_task_name = json.loads(unique_task_name_json)
-    # get_list_of_attachments = Email_Attachements.objects.filter(email_name = unique_task_name)
     is_scheduled= True
     # PRT_Email_System.send_email(to_email_list, cc_email_list, bcc_email_list, subject, mail_body,is_scheduled,get_list_of_attachments)
     
